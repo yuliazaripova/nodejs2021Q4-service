b60f1767ea2813fca63c8d1e66dc9ae9eced607f
Iuliia Zaripova <iuliia_zaripova@epam.com>
2021-12-23 18:03:44 +0400

    feat: add logger

---
+++ src/logger/logger.ts
@@ -0,0 +1,41 @@
+import { FastifyRequest } from "fastify"
+import pino from "pino"
+
+const initLogger = (level = "info") => pino({
+    level,
+    // customLevels: {
+    //     foo: 35
+    //   },
+    // useOnlyCustomLevels: true,
+    serializers: {
+      req (request: FastifyRequest) {
+        return { 
+          method: request.method,
+          url: request.url,
+          params: request.params,
+          hostname: request.hostname,
+        }
+      }
+   },
+    transport: {
+      targets: [{
+        level: 'info',
+        target: 'pino-pretty', // must be installed separately
+        options: {
+                 colorize: true
+              }
+      }, {
+        level: 'info',
+        target: 'pino/file',
+        options: { destination: `${__dirname}/../logs/info.log` }
+      },
+      {
+        level: 'error',
+        target: 'pino/file',
+        options: { destination: `${__dirname}/../logs/error.log` }
+      }
+    ]
+    }
+  })
+
+  export default initLogger
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,71 @@
-import Fastify from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import CONFIG from './common/config';
 import boardsRoutes from './resources/boards/boards.routes';
 import userRoutes from './resources/users/user.routes';
 import tasksRoutes from './resources/tasks/tasks.routes';
+import initLogger from './logger/logger';
 
-const fastify = Fastify({
-  logger: true
+
+// const a = pino({
+//   level: "info",  
+//   transport: {
+//     target: 'pino-pretty',
+//     options: {
+//       colorize: true
+//     }
+//   }
+// }, pino.multistream(streams, opts))
+
+
+// const log = pino({
+//   level: "info",
+//   serializers: {
+//     req (request) {
+//       return { 
+//         method: request.method,
+//         url: request.url,
+//         params: request.params,
+//         hostname: request.hostname,
+//         remoteAddress: request.remoteAddress 
+//       }
+//     }
+//  },
+//   transport: {
+//     targets: [{
+//       level: 'info',
+//       target: 'pino-pretty', // must be installed separately
+//       options: {
+//                colorize: true
+//             }
+//     }, {
+//       level: 'info',
+//       target: 'pino/file',
+//       options: { destination: `${__dirname}/logs/info.log` }
+//     },
+//     {
+//       level: 'error',
+//       target: 'pino/file',
+//       options: { destination: `${__dirname}/logs/error.log` }
+//     }
+//   ]
+//   }
+// })
+
+const logger = initLogger(CONFIG.LOGGER_LEVEL)
+const fastify = require('fastify')({
+  logger
+})
+
+fastify.addHook('preHandler', (req: FastifyRequest, _reply: FastifyReply, done: () => void) => {
+  if (req.body) {
+    req.log.info({ body: req.body}, 'parsed body')
+  }
+  done()
 })
 
 fastify.register(userRoutes)
 fastify.register(boardsRoutes)
 fastify.register(tasksRoutes)
-
 
 /**
  * Start fastify app
@@ -20,8 +74,9 @@ fastify.register(tasksRoutes)
 const start = async () => {
   try {
     await fastify.listen(CONFIG.PORT as string)
+  
   } catch (err) {
-    fastify.log.error(err)
+  //  log.error(err)
     process.exit(1)
   }
 }
